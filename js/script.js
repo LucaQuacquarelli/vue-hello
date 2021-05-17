78aea0007f7d0255557e13dbabd6177c3246e4c2
LucaQuacquarelli <lucaq82@libero.it>
2021-05-17 13:27:23 +0200

    Stampato messaggio in h1 con i data

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+var app = new Vue( {
+    
+    el: "#root",
+    data: {
+        message: "Hello World!!!"
+    }
+
+})
